3192cc6089cef3d6edf70c41e647cd1c3c678bf5
Malena Guallar <malena.guallar@gmail.com>
2024-03-12 14:40:37 +0100

    [feat] Created fonts file and importing fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import RegistrationBar from './components/RegistrationBar/RegistrationBar'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div class="app">
+      <h4>odace +</h4>
+      <h1>The new standard to create social media video content</h1>
+      <p>Combining top creatives with best-in-class AI solutions to help you produce high quality videos. You imagine. We do the rest.</p>
+      <RegistrationBar />
+    </div>
   )
 }
 
---
+++ src/components/RegistrationBar/RegistrationBar.jsx
@@ -0,0 +1,14 @@
+import './RegistrationBar.css'
+
+function RegistrationBar() {
+    return (
+
+        <div class="resgistrationBar">
+            <input placeholder="Your email" />
+            <button>Pre-register</button>
+        </div>
+    )
+
+}
+
+export default RegistrationBar
