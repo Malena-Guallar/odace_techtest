79a4b0f8f9c336c4bd73a5c74230b60edd6562e7
Malena Guallar <malena.guallar@gmail.com>
2024-03-13 17:19:31 +0100

    [feat] Added carousel for partners' logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import RegistrationBar from './components/RegistrationBar/RegistrationBar'
+import Caroussel from './components/RegistrationBar/Caroussel/Caroussel'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <p className='text_header'>Combining top creatives with best-in-class AI solutions to help you produce high quality videos.<br></br> <b>You imagine. We do the rest.</b></p>
       <RegistrationBar />
       <div className='separation_line'/>
+      <Caroussel />
     </div>
   )
 }
